cbfdd7df766215db84ec10bab395bdf9c95e8da4
TahzeebHussainLone <tahzeeb.lone@student.ehb.be>
2024-11-13 23:42:34 +0100

    verbetered

--- oefGD/generative-design.js
+++ oefGD/generative-design.js
@@ -6,17 +6,19 @@ import * as Noise from "../../scripts/noise.js";
 let width = context.canvas.width;
 let height = context.canvas.height;
 
-
 drawPosters();
 function drawPosters() {
-    const width = 250;
-    const height = 350;
-    const Xrect = (200,550,1000,1350);
-    const yrect = (50,550);
+	const width = 250;
+	const height = 350;
 
-    context.fillStyle = "black";
-    context.fillRect(Xrect, yrect, width, height);
-    
+	context.fillStyle = "black";
+	context.fillRect(200, 50, width, height);
+	context.fillRect(550, 50, width, height);
+	context.fillRect(1000, 50, width, height);
+	context.fillRect(1350, 50, width, height);
 
+	context.fillRect(200, 550, width, height);
+	context.fillRect(550, 550, width, height);
+	context.fillRect(1000, 550, width, height);
+	context.fillRect(1350, 550, width, height);
 }
-
