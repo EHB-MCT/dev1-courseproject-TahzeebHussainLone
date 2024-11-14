333bc930a1175fad8fb6c4b5e8cbe1d3f7576df0
TahzeebHussainLone <tahzeeb.lone@student.ehb.be>
2024-11-14 01:22:24 +0100

    Ik denk dat dit goed is?

--- oefGD/generative-design.js
+++ oefGD/generative-design.js
@@ -3,9 +3,11 @@
 import context from "../../scripts/context.js";
 import * as Utils from "../../scripts/utils.js";
 import * as Noise from "../../scripts/noise.js";
+
 let width = context.canvas.width;
 let height = context.canvas.height;
 
+//base layer van posters
 drawPosters();
 function drawPosters() {
 	const width = 250;
@@ -23,18 +25,66 @@ function drawPosters() {
 	context.fillRect(1350, 550, width, height);
 }
 
+//plaats waar de vormen op moeten
 function addShapes() {
-    const width = 250;
+	const width = 250;
 	const height = 350;
 
-    drawRandomShapes(200, 50, width, height);
-    drawRandomShapes(550, 50, width, height);
-    drawRandomShapes(1000, 50, width, height);
-    drawRandomShapes(1350, 50, width, height);
+	drawRandomShapes(200, 50, width, height);
+	drawRandomShapes(550, 50, width, height);
+	drawRandomShapes(1000, 50, width, height);
+	drawRandomShapes(1350, 50, width, height);
 
-    drawRandomShapes(200, 550, width, height);
-    drawRandomShapes(550, 550, width, height);
-    drawRandomShapes(1000, 550, width, height);
-    drawRandomShapes(1350, 550, width, height);
+	drawRandomShapes(200, 550, width, height);
+	drawRandomShapes(550, 550, width, height);
+	drawRandomShapes(1000, 550, width, height);
+	drawRandomShapes(1350, 550, width, height);
 }
 
+// vormen kleur en grootte
+function drawRandomShapes(x, y, width, height) {
+	const amountShape = 3;
+
+	for (let i = 0; i < amountShape; i++) {
+		const randomColor = getRandomColor();
+		context.fillStyle = randomColor;
+
+		const randomX = x + Math.random() * width;
+		const randomY = y + Math.random() * height;
+
+		const drawShapes = Math.random();
+
+		if (drawShapes < 0) {
+			drawRectangle(randomX, randomY, 50, 50);
+		} else if (drawShapes < 1) {
+			drawCircle(randomX, randomY, 30);
+		} else {
+			drawLine(randomX, randomY);
+		}
+	}
+}
+
+//rechthoek
+function drawRectangle(x, y, width, height) {
+	context.beginPath();
+	context.rect(x, y, width, height);
+	context.fill();
+}
+
+//cirkel
+function drawCircle(x, y, straal) {
+	context.arc(x, y, straal, 0, 2 * Math.PI);
+	context.fill();
+}
+
+// lijn
+function drawLine(x, y) {
+	const length = Math.random() * 50;
+	const eindX = x + length;
+	const eindY = y + length;
+
+	context.beginPath();
+	context.moveTo(x, y);
+	context.lineTo(eindX, eindY);
+	context.stroke();
+}
